3fc323d86694b9cb0741edebab59f912e60230b8
Siarhei-Douhi <serrik23@gmail.com>
2022-09-03 17:27:39 +0300

    fix: change InstagramPost. Add mock.ts and folders -  assets, types

--- hw-react-component/src/App.tsx
+++ hw-react-component/src/App.tsx
@@ -4,9 +4,9 @@ import { Input } from "./components/Input";
 import { UserCard } from "./components/UserCard";
 import { Title } from "./components/Title";
 import { Clicker } from "./components/Clicker";
-import { ItemPost } from "./components/InstagramPost/ItemPost";
 import { ListPost } from "./components/InstagramPost/ListPosts";
 import { EmojiList } from "./components/Emoji/EmojiList";
+import { posts } from "./mock";
 
 function App() {
   const onClickBtn1 = () => {
@@ -27,29 +27,7 @@ function App() {
       <UserCard userName="Artem Malkin" isDark={false} />
       <UserCard userName="Ivanov" isDark={true} />
       <Clicker />
-      <div style={{ display: "flex ", marginBottom: "30px" }}>
-        <ItemPost
-          image="https://avatarko.ru/img/kartinka/9/robot_medved_8715.jpg"
-          author="Petrov"
-          text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
-          title="What is Lorem Ipsum?"
-          date={"28.08.2022"}
-        />
-        <ItemPost
-          image="https://avatarko.ru/img/kartinka/2/multfilm_robot_1481.jpg"
-          author="Ivanov"
-          text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
-          title="What is Lorem Ipsum?"
-          date={"28.08.2022"}
-        />
-        <ItemPost
-          author="Sidorov"
-          text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-          title="What is Lorem Ipsum?"
-          date={"28.08.2022"}
-        />
-      </div>
-      <ListPost />
+      <ListPost posts={posts} />
       <EmojiList />
     </div>
   );
